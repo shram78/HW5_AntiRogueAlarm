6e5f8f3fefce54e07160663865851c342bcc3334
shram78 <ra00275@gmail.com>
2021-09-10 01:02:11 +0300

    first commit

---
+++ Assets/Scripts/ManageSignaling.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManageSignaling : MonoBehaviour
+{
+    [SerializeField] private SpriteRenderer _renderer;
+    [SerializeField] private Animator _animator;
+
+    public void OpenDoor()
+    {
+        _renderer.color = Color.white;
+    }
+
+    public void CloseDoor()
+    {
+        _renderer.color = Color.clear;
+    }
+
+    public void LightSignalingOn()
+    {
+        _animator.Play("SignalingAlarm");
+    }
+
+    public void LightSignalingOff()
+    {
+        _animator.Play("SignalingOff");
+    }
+}
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+
+    private Animator _animator;
+
+    private void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(_speed * Time.deltaTime, 0, 0);
+            GetComponent<SpriteRenderer>().flipX = false;
+            _animator.SetTrigger("Run");
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(_speed * Time.deltaTime * -1, 0, 0);
+            GetComponent<SpriteRenderer>().flipX = true;
+            _animator.SetTrigger("Run");
+        }
+    }
+}
