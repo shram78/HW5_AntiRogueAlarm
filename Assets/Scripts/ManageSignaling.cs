7207391d7b0fd70296a1ba4867e6409bf40222d8
shram78 <ra00275@gmail.com>
2021-09-15 02:12:19 +0300

    second commit
    fix:
    1.) Поправил анимации ГГ и сигналки;
    2.) Избавился от строковых литералов во всех классах;
    3.) Убрал GetComponent из Update
    4.) Убрал дублеж кода в корутине
    5.) Воспользовался методом Mathf.MoveTowards в корутине для плавного изменения громкости;
    6.) всю логику работы сигнализаций (дверь, свет, звук) вынес в отдельный класс ManageSignaling.

--- Assets/Scripts/ManageSignaling.cs
+++ Assets/Scripts/ManageSignaling.cs
@@ -7,9 +7,18 @@ public class ManageSignaling : MonoBehaviour
     [SerializeField] private SpriteRenderer _renderer;
     [SerializeField] private Animator _animator;
 
+    private AudioSource _audioSource;
+    private const string _doorOpen = "isDoorOpen";
+
+    private void Start()
+    {
+        _audioSource = GetComponent<AudioSource>();
+    }
+
     public void OpenDoor()
     {
         _renderer.color = Color.white;
+        _audioSource.volume = 0f;
     }
 
     public void CloseDoor()
@@ -19,11 +28,33 @@ public class ManageSignaling : MonoBehaviour
 
     public void LightSignalingOn()
     {
-        _animator.Play("SignalingAlarm");
+        _animator.SetBool(_doorOpen, true);
     }
 
     public void LightSignalingOff()
     {
-        _animator.Play("SignalingOff");
+        _animator.SetBool(_doorOpen, false);
+    }
+
+    public void SoundSignalingOn()
+    {
+        _audioSource.Play();
+        StartCoroutine(VolumeChange(1));
+    }
+
+    public void SoundSignalingOff()
+    {
+        StartCoroutine(VolumeChange(-1));
+    }
+
+    private IEnumerator VolumeChange(int volumeUpDown)
+    {
+        for (int i = 0; i < 1000; i++)
+        {
+            _audioSource.volume = Mathf.MoveTowards(_audioSource.volume, 1f, 0.5f * Time.deltaTime * volumeUpDown);
+            Debug.Log(_audioSource.volume);
+
+            yield return null;
+        }
     }
 }
--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -6,27 +6,32 @@ public class Movement : MonoBehaviour
 {
     [SerializeField] private float _speed;
 
+    private SpriteRenderer _spriteRenderer;
     private Animator _animator;
+    private const string _isRunning = "isRunning";
 
     private void Start()
     {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
         _animator = GetComponent<Animator>();
     }
 
     private void Update()
     {
+        _animator.SetBool(_isRunning, false);
+
         if (Input.GetKey(KeyCode.D))
         {
             transform.Translate(_speed * Time.deltaTime, 0, 0);
-           // GetComponent<SpriteRenderer>().flipX = false;
-            _animator.SetTrigger("Run");
+            _spriteRenderer.flipX = false;
+            _animator.SetBool(_isRunning, true);
         }
 
         if (Input.GetKey(KeyCode.A))
         {
             transform.Translate(_speed * Time.deltaTime * -1, 0, 0);
-          //  GetComponent<SpriteRenderer>().flipX = true;
-            _animator.SetTrigger("Run");
+            _spriteRenderer.flipX = true;
+            _animator.SetBool(_isRunning, true);
         }
     }
 }
